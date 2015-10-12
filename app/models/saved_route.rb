e7be2049731735fb8efc90c2dd443f5a6f347fbc
blaster25 <axelcordova120@gmail.com>
2015-10-12 21:31:26 +0800

    added update functionality to api

--- app/models/saved_route.rb
+++ app/models/saved_route.rb
@@ -1,2 +1,6 @@
 class SavedRoute < ActiveRecord::Base
+	validates :start, presence: true
+	validates :end, presence: true
+	validates :route, presence: true
+	validates :fare, presence: true
 end
--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module JeepNiCagayan
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options, :delete]
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :delete, :put]
       end
     end
     
